d85d1d9993b089e17e5eea3419921a9efe1fe92e
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-05-02 19:55:33 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,9 @@
+class Post
+attr_accessor :title, :author
+
+  def initalize(title, author)
+    @title = title
+    @author = author
+  end
+  
+end
---
+++ lib/song.rb
@@ -0,0 +1,9 @@
+class Song
+  attr_accessor :title, :artist
+
+  def initalize(title, artist)
+    @title = title
+    @artist = artist
+  end
+  
+end
